fa2a7e9a6847560fcfb7c93bcc5cf4b2ac2a07b1
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2022-11-13 10:52:31 -0600

    Change Config to own Strings.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{env, fs, io};
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let config =
-        Config::parse_args(&args).map_err(|err| io::Error::new(io::ErrorKind::NotFound, err))?;
+    let config = Config::parse_args(env::args())
+        .map_err(|err| io::Error::new(io::ErrorKind::NotFound, err))?;
     let search_space = fs::read_to_string(config.filename)?;
-    if let true = search_space.contains(config.pattern) {
+    if let true = search_space.contains(&config.pattern) {
         println!("Match found!");
         Ok(())
     } else {
@@ -15,16 +14,17 @@ fn main() -> io::Result<()> {
 }
 
 #[derive(Debug)]
-struct Config<'a> {
-    pattern: &'a String,
-    filename: &'a String,
+struct Config {
+    pattern: String,
+    filename: String,
 }
 
-impl<'a> Config<'a> {
+impl Config {
     /// Parse a vec of args into a struct of config values
-    fn parse_args(args: &'a [String]) -> Result<Self, String> {
-        let pattern = args.get(1).ok_or("Pattern arg missing!")?;
-        let filename = args.get(2).ok_or("Filename arg missing!")?;
+    fn parse_args<'a>(mut args: impl Iterator<Item = String>) -> Result<Self, &'a str> {
+        args.next();
+        let pattern: String = args.next().ok_or("Arg missing: pattern")?;
+        let filename: String = args.next().ok_or("Arg missing: filename")?;
 
         Ok(Config { pattern, filename })
     }
@@ -37,11 +37,11 @@ mod tests {
     #[test]
     fn struct_args_parses_vec_into_pattern_and_filename() {
         let expected = ["bin/name", "find me", "filename.txt"];
-        let args: Vec<String> = expected.iter().map(|&v| String::from(v)).collect();
+        let args = expected.iter().map(|&v| String::from(v));
 
-        let parsed = Config::parse_args(&args).unwrap();
+        let parsed = Config::parse_args(args).unwrap();
 
-        assert_eq!(parsed.pattern, &String::from(expected[1]),);
-        assert_eq!(parsed.filename, &String::from(expected[2]),);
+        assert_eq!(parsed.pattern, String::from(expected[1]),);
+        assert_eq!(parsed.filename, String::from(expected[2]),);
     }
 }
